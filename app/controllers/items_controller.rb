9ce922bfb358577141f188f2e3aba1595b9c7b0f
Michael <mtrix92@gmail.com>
2022-09-16 18:33:20 -0400

    final commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
           redirect to '/login'
         end
       end
-
+    
       get '/items/new' do
         if logged_in?
           erb :'items/create_item'
@@ -32,4 +32,61 @@ class ItemsController < ApplicationController
           redirect to '/login'
         end
       end
+    
+      get '/items/:id' do
+        if logged_in?
+          @item = Item.find_by_id(params[:id])
+          erb :'items/show_item'
+        else
+          redirect to '/login'
+        end
+      end
+    
+      get '/items/:id/edit' do
+        if logged_in?
+          @item = Item.find_by_id(params[:id])
+          if @item && @item.user == current_user
+            erb :'items/edit_item'
+          else
+            redirect to '/items'
+          end
+        else
+          redirect to '/login'
+        end
+      end
+    
+      patch '/items/:id' do
+        if logged_in?
+          if params[:name] == ""
+            redirect to "/items/#{params[:id]}/edit"
+          else
+            @item = Item.find_by_id(params[:id])
+            if @item && @item.user == current_user
+              if @item.update(name: params[:name])
+                redirect to "/items/#{@item.id}"
+              else
+                redirect to "/items/#{@item.id}/edit"
+              end
+            else
+              redirect to '/items'
+            end
+          end
+        else
+          redirect to '/login'
+        end
+      end
+    
+      delete '/items/:id/delete' do
+        if logged_in?
+          @item = Item.find_by_id(params[:id])
+          if @item && @item.user == current_user
+            @item.delete
+          end
+          redirect to '/items'
+        else
+          redirect to '/login'
+        end
+      end
+
+
 end
