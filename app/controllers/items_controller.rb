7201a2906a60f6056ea92af5820b21e037c55d6a
Michael <mtrix92@gmail.com>
2022-09-16 18:22:09 -0400

    users can log in/out and add items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,35 @@
 class ItemsController < ApplicationController
+    get '/items' do
+        if logged_in?
+          @items = Item.all
+          erb :'items/items'
+        else
+          redirect to '/login'
+        end
+      end
+
+      get '/items/new' do
+        if logged_in?
+          erb :'items/create_item'
+        else
+          redirect to '/login'
+        end
+      end
+    
+      post '/items' do
+        if logged_in?
+          if params[:name] == ""
+            redirect to "/items/new"
+          else
+            @item = current_user.items.build(name: params[:name])
+            if @item.save
+              redirect to "/items"
+            else
+              redirect to "/items/new"
+            end
+          end
+        else
+          redirect to '/login'
+        end
+      end
 end
